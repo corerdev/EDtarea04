e87d1ed9699023d62f04a82845d31cbbc165ab81
Core <Core@LAPTOP-K19JUHSA>
2023-02-23 06:37:28 +0100

    3.	Utiliza las funciones de refactorización  de código, utilizando Eclipse , para renombrar aquellos identificadores que consideres no tienen un nombre adecuado, especificando detalladamente como lo haces y por qué los eliges. Sube las modificaciones al repositorio de Github que has creado para el proyecto de Eclipse, siendo el comentario el enunciado de este ejercicio.

--- EDtarea04/src/notas/Notas.java
+++ EDtarea04/src/notas/Notas.java
@@ -1,5 +1,28 @@
 package notas;
 
+import java.util.Scanner;
+
 public class Notas {
 
+public static void main(String[] args) {
+Scanner reader = new Scanner(System.in);// Para pedir un dato numérico por teclado
+int nota = 0;
+String resultado = "";
+System.out.print("Introduzca el valor de una calificación: ");
+nota = reader.nextInt();
+
+if (nota >= 0 && nota < 5)
+resultado = "Suspenso";
+else if (nota >= 5 && nota < 7)
+resultado = "Aprobado";
+else if (nota >= 7 && nota <9)
+resultado = "Notable";
+else if (nota >= 9 && nota <= 10)
+resultado = "Sobresaliente";
+else
+resultado = "El valor de la calificación introducida no es correcta";
+
+System.out.println(resultado);
+reader.close();
+}
 }
