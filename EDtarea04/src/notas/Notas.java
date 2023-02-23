65d0d56297d6e2ae6abedd236b2c477afb62f4a1
Core <Core@LAPTOP-K19JUHSA>
2023-02-23 06:43:41 +0100

    4.	Utiliza las funciones de refactorización para sustituir el código que se encarga de especificar la calificación en función de la nota por un método que llamarás "cali", que será público, de forma que se incluyan los comentarios para la documentación si ello es posible. Muestra cómo se hace en los dos entornos. (En Eclipse). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio.

--- EDtarea04/src/notas/Notas.java
+++ EDtarea04/src/notas/Notas.java
@@ -11,18 +11,28 @@ String resultado = "";
 System.out.print("Introduzca el valor de una calificación: ");
 nota = reader.nextInt();
 
-if (nota >= 0 && nota < 5)
-resultado = "Suspenso";
-else if (nota >= 5 && nota < 7)
-resultado = "Aprobado";
-else if (nota >= 7 && nota <9)
-resultado = "Notable";
-else if (nota >= 9 && nota <= 10)
-resultado = "Sobresaliente";
-else
-resultado = "El valor de la calificación introducida no es correcta";
+resultado = cali(nota);
 
 System.out.println(resultado);
 reader.close();
+}
+
+/**
+ * @param nota
+ * @return
+ */
+public static String cali(int nota) {
+	String resultado;
+	if (nota >= 0 && nota < 5)
+	resultado = "Suspenso";
+	else if (nota >= 5 && nota < 7)
+	resultado = "Aprobado";
+	else if (nota >= 7 && nota <9)
+	resultado = "Notable";
+	else if (nota >= 9 && nota <= 10)
+	resultado = "Sobresaliente";
+	else
+	resultado = "El valor de la calificación introducida no es correcta";
+	return resultado;
 }
 }
